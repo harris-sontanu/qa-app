02c53ba450f937a02438c07da4862dabc5374a21
Harris Sontanu <harris.sontanu@gmail.com>
2022-04-20 15:13:20 +0800

    Define Question and User model relationship

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
 
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function title(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => [
+                'title' => Str::title($value),
+                'slug'  => Str::slug($value)
+            ]
+        );
+    }
 }

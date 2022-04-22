1d4be57b045bab40d9364a568f11e63de29a8825
Harris Sontanu <harris.sontanu@gmail.com>
2022-04-22 18:03:33 +0800

    Votes, views, and answers markup

--- resources/views/pages/question.blade.php
+++ resources/views/pages/question.blade.php
@@ -29,24 +29,39 @@
                             @foreach ($questions as $question)
                                 
                                 <ul class="topic list-unstyled row mx-0 justify-content-between align-items-center border-top-0">
-                                    <li class="entry mb-0 col-sm-10">
-                                        <h3 class="mb-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                        {{ Str::limit($question->body, 200) }}
-                                        <div class="entry-meta mt-1">
-                                            <ul>
-                                                <li><a href="#">{{ $question->created_at->diffForHumans() }}</a></li>
-                                                <li><a href="#">{{ $question->answers }} Replies</a></li>
-                                                @if (!empty($question->best_answer_id))                                                     
-                                                    <li><a href="#" class="badge bg-success text-white">Solved</a></li>
-                                                @endif
-                                            </ul>
+                                    <li class="entry mb-0">
+                                        <div class="row">
+                                            <div class="col-lg-2 d-flex flex-column text-end pt-1">
+                                                <div class="vote pb-2">
+                                                    <strong>{{ $question->votes }}</strong> votes
+                                                </div>
+                                                <div class="status pb-2">
+                                                    <strong>{{ $question->answers }}</strong> answers
+                                                </div>
+                                                <div class="view text-muted">
+                                                    <strong>{{ $question->views }}</strong> views
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-10">
+                                                <h3 class="mb-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                                {{ Str::limit($question->body, 200) }}
+                                                <div class="mt-2 d-flex justify-content-between">
+                                                    <ul class="list-inline">
+                                                        <li class="list-inline-item"><a href="#"><span class="badge bg-secondary">javascript</span></a></li>
+                                                        <li class="list-inline-item"><a href="#"><span class="badge bg-secondary">laravel</span></a></li>
+                                                        <li class="list-inline-item"><a href="#"><span class="badge bg-secondary">php</span></a></li>
+                                                    </ul>
+                                                    <div class="text-end small">
+                                                        <a href="#" class="text-default">
+                                                            <img class="align-text-bottom me-1" alt="{{ $question->user->name }}" src="demos/forum/images/user.png" width="16" height="16">
+                                                            {{ $question->user->name }}
+                                                        </a>
+                                                        <span class="text-muted">asked {{ $question->created_at->diffForHumans() }}</span>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </li>
-                                    <li class="col-sm-2">
-										<div class="bbp-author">
-											<a href="#"><img alt="{{ $question->user->name }}" src="demos/forum/images/user.png"></a>
-										</div>
-									</li>
                                 </ul>
 
                             @endforeach

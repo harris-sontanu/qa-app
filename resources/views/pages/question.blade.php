815c08c67a9667ef2e994a88a1419f5cb5040d1b
Harris Sontanu <harris.sontanu@gmail.com>
2022-04-22 21:39:01 +0800

    Set style for answers information

--- resources/views/pages/question.blade.php
+++ resources/views/pages/question.blade.php
@@ -31,13 +31,23 @@
                                 <ul class="topic list-unstyled row mx-0 justify-content-between align-items-center border-top-0">
                                     <li class="entry mb-0">
                                         <div class="row">
-                                            <div class="col-lg-2 d-flex flex-column text-end pt-1">
-                                                <div class="vote pb-2">
+                                            <div class="col-lg-2 d-flex flex-column text-end pt-1 small">
+                                                <div class="vote mb-2">
                                                     <strong>{{ $question->votes }}</strong> votes
                                                 </div>
-                                                <div class="status pb-2">
-                                                    <strong>{{ $question->answers }}</strong> answers
-                                                </div>
+                                                @if ($question->answers > 0 && !empty($question->best_answer_id))   
+                                                    <div class="status mb-2">
+                                                        <span class="bg-success rounded text-white px-2 py-1"><i class="icon-check me-1"></i><strong>{{ $question->answers }}</strong> answers</span>
+                                                    </div>          
+                                                @elseif ($question->answers > 0 && empty($question->best_answer_id))
+                                                    <div class="status mb-2">
+                                                        <span class="border border-success rounded text-success px-2 py-1"><strong>{{ $question->answers }}</strong> answers</span>
+                                                    </div> 
+                                                @else                                       
+                                                    <div class="status mb-2">
+                                                        <strong>{{ $question->answers }}</strong> answers
+                                                    </div>                                                     
+                                                @endif
                                                 <div class="view text-muted">
                                                     <strong>{{ $question->views }}</strong> views
                                                 </div>
